d19750ece504b6b6026c56f0c6c0c050de7c7f8a
Mridula Shanker <Mridula Shanker@LAPTOP-T2PHJ0FM>
2018-09-16 19:44:33 -0400

    Comment2

--- Mridula-Project2018/src/Learning/HelloMridula.java
+++ Mridula-Project2018/src/Learning/HelloMridula.java
@@ -3,7 +3,7 @@ package Learning;
 public class HelloMridula {
 
 	public static void main(String[] args) {
-		String name = "Mridula";
+		String name = "Mridula-Shruti";
 		System.out.println("Hello "+name);
 	}
 
