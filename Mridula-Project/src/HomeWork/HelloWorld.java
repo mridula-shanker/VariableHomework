e4c94e9e356adf3df7f98ec540ae8b746c500bf6
Mridula Shanker <Mridula Shanker@LAPTOP-T2PHJ0FM>
2018-09-16 19:41:34 -0400

    09162018 - Comment 1

--- Mridula-Project/src/HomeWork/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package HomeWork;
-
-public class HelloWorld {
-	public static void main(String args[]){
-		String name = "Mridula-Shruti From PC";
-		System.out.println("Hello World from: "+name);
-	}
-}
---
+++ Mridula-Project2018/src/Learning/HelloMridula.java
@@ -0,0 +1,10 @@
+package Learning;
+
+public class HelloMridula {
+
+	public static void main(String[] args) {
+		String name = "Mridula";
+		System.out.println("Hello "+name);
+	}
+
+}
