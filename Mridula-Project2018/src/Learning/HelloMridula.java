d3cbbb3de8a9b87dbd3c881a79bace904ae7a7c1
mridula-shanker <42977368+mridula-shanker@users.noreply.github.com>
2018-09-16 19:47:14 -0400

    Update HelloMridula.java
    Comment 4

--- Mridula-Project2018/src/Learning/HelloMridula.java
+++ Mridula-Project2018/src/Learning/HelloMridula.java
@@ -3,7 +3,7 @@ package Learning;
 public class HelloMridula {
 
 	public static void main(String[] args) {
-		String name = "Mridula-Shruti 2";
+		String name = "Mridula-Shruti 3";
 		System.out.println("Hello "+name);
 	}
 
