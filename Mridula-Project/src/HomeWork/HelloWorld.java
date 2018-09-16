a7c78d7765977d8f3c8986eae642af819a668028
mridula-shanker <42977368+mridula-shanker@users.noreply.github.com>
2018-09-16 19:11:44 -0400

    Update HelloWorld.java
    09162018 - Day 1 Demo (Test 2)

--- Mridula-Project/src/HomeWork/HelloWorld.java
+++ Mridula-Project/src/HomeWork/HelloWorld.java
@@ -2,7 +2,7 @@ package HomeWork;
 
 public class HelloWorld {
 	public static void main(String args[]){
-		String name = "Mridula-Shruti";
+		String name = "Mridula-Shruti (Shankar)";
 		System.out.println("Hello World from: "+name);
 	}
 }
